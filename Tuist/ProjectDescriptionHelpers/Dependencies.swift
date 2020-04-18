7a3716778dcf2b7be8dd38d0acad9350b6ad1462
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-18 14:41:50 +0200

    tuist - added dependencies helper

---
+++ Tuist/ProjectDescriptionHelpers/Dependencies.swift
@@ -0,0 +1,15 @@
+import ProjectDescription
+
+public extension TargetDependency {
+    // App
+    static let appCore: TargetDependency = .project(target: "AppCore", path: .relativeToRoot("Modules/AppCore"))
+    // Features
+    static let characterDetailKit: TargetDependency = .project(target: "CharacterDetailKit", path: .relativeToRoot("Modules/CharacterDetailKit"))
+    static let characterListKit: TargetDependency = .project(target: "CharacterListKit", path: .relativeToRoot("Modules/CharacterListKit"))
+    // Core Dependencies
+    static let dataProvidersKit: TargetDependency = .project(target: "DataProvidersKit", path: .relativeToRoot("Modules/DataProvidersKit"))
+    static let displayKit: TargetDependency = .project(target: "DisplayKit", path: .relativeToRoot("Modules/DisplayKit"))
+    static let marvelClient: TargetDependency = .project(target: "MarvelClient", path: .relativeToRoot("Modules/MarvelClient"))
+    static let navigatorKit: TargetDependency = .project(target: "NavigatorKit", path: .relativeToRoot("Modules/NavigatorKit"))
+    static let support: TargetDependency = .project(target: "Support", path: .relativeToRoot("Modules/Support"))
+}

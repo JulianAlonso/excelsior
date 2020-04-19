a7cfcc327e80f7b408a5b7c98cd2e607889add21
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-19 19:24:42 +0200

    promises - added work initializer

--- Libraries/Promises/Sources/Promises.swift
+++ Libraries/Promises/Sources/Promises.swift
@@ -5,7 +5,23 @@ public final class Promise<T, E: Error> {
     private var state: State<T, E>
     
     public init() {
-        self.state = .pending(future: [])
+        self.state = .pending(futures: [])
     }
+    
+    public convenience init(on queue: Queue, _ work: @escaping (Future<T, E>) -> Void) {
+        self.init()
+        queue.execute {
+            work(Future(fulfill: self.fulfill, reject: self.reject))
+        }
+    }
+
+    private func fulfill(with value: T) {
+        
+    }
+
+    private func reject(with error: E) {
+        
+    }
+
     
 }

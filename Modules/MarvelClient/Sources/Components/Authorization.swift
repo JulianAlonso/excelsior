947d6b304a5badc85f9ac5c1288cf908d726e02d
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-21 00:53:49 +0200

    client - Authorization now Authorizating

--- Modules/MarvelClient/Sources/Components/Authorization.swift
+++ Modules/MarvelClient/Sources/Components/Authorization.swift
@@ -17,3 +17,20 @@ public struct Authorization {
         self.privateKey = privateKey
     }
 }
+
+private extension Authorization {
+    var parameters: [String: String] {
+        let timeStamp = "\(Date().timeIntervalSince1970)"
+        return [
+            "apikey": publicKey,
+            "hash": "\(timeStamp)\(privateKey)\(publicKey)".md5,
+            "ts": timeStamp
+        ]
+    }
+}
+
+extension Authorization: Authorizating {
+    public func authorize(endpoint: Endpoint) -> Endpoint {
+        endpoint.adding(parameters: parameters)
+    }
+}

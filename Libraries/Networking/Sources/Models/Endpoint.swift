82b126e8f9e554ea029ac5d39dfbf075e582c31a
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-20 20:14:05 +0200

    client - Created basic Endpoint

---
+++ Libraries/Networking/Sources/Models/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Juli Alonso on 20/04/2020.
+//
+
+import Foundation
+
+public struct Endpoint {
+    let endpoint: String
+    var paramters: [String: Any]
+}

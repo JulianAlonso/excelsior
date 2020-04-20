8e4607a2cd811c0b0cfd6532232a5575108176bd
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-20 20:24:20 +0200

    client - Endpoint encodable to a host

--- Libraries/Networking/Sources/Models/Endpoint.swift
+++ Libraries/Networking/Sources/Models/Endpoint.swift
@@ -11,3 +11,14 @@ public struct Endpoint {
     let endpoint: String
     var paramters: [String: Any]
 }
+
+extension Endpoint {
+    func encode(to host: URL) -> URL {
+        let url = host.appendingPathComponent(endpoint)
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        
+        components.queryItems = paramters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
+        
+        return components.url!
+    }
+}

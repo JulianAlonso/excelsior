9e3e024d347ef33472bd630d3d138828f7a04c55
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-20 19:52:25 +0200

    client - Clean up the package

--- Libraries/Networking/Package.swift
+++ Libraries/Networking/Package.swift
@@ -1,28 +1,16 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "Networking",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Networking",
-            targets: ["Networking"]),
+    platforms: [
+        .iOS(.v10)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(name: "Networking", targets: ["Networking"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Networking",
-            dependencies: []),
-        .testTarget(
-            name: "NetworkingTests",
-            dependencies: ["Networking"]),
+        .target(name: "Networking", dependencies: [], path: "Sources"),
+        .testTarget(name: "NetworkingTests", dependencies: ["Networking"], path: "Tests"),
     ]
 )
--- Libraries/Networking/Sources/Networking/Networking.swift
+++
@@ -1,3 +0,0 @@
-struct Networking {
-    var text = "Hello, World!"
-}

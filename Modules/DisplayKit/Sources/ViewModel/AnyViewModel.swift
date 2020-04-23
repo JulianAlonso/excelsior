34a3d69627bafb847005d5459889e92d777b1987
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-23 20:51:47 +0200

    features - added AnyViewModel

---
+++ Modules/DisplayKit/Sources/ViewModel/AnyViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AnyViewModel.swift
+//  DisplayKit
+//
+//  Created by Juli Alonso on 23/04/2020.
+//
+
+import Foundation
+
+public final class AnyViewModel<State, Action> {
+    
+    var state: State {
+        get { viewModel.state }
+    }
+    private let viewModel: ViewModel<State, Action>
+    
+    public init(viewModel: ViewModel<State, Action>) {
+        self.viewModel = viewModel
+    }
+    
+    public func handle(_ action: Action) {
+        viewModel.handle(action)
+    }
+    
+}

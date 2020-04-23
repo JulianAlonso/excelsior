e07323b9a9ea4a3e39c9ff96ba2b84eeb9e0c96f
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-23 20:55:48 +0200

    features - any view hashable

--- Modules/DisplayKit/Sources/ViewModel/AnyView.swift
+++ Modules/DisplayKit/Sources/ViewModel/AnyView.swift
@@ -10,13 +10,25 @@ import Foundation
 public final class AnyView<State>: StatefulView {
     
     private let _render: (State) -> Void
+    private let identifier: String
     
     public init<V: StatefulView>(view: V) where V.State == State {
         _render = { view.render(state: $0) }
+        identifier = String(describing: view)
     }
     
     public func render(state: State) {
         DispatchQueue.main.async { self._render(state) }
     }
     
+}
+
+extension AnyView: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+    
+    public static func == (lhs: AnyView, rhs: AnyView) -> Bool {
+        lhs.identifier == rhs.identifier
+    }
 }

60050a177db6ecbb26c97f0f3a3f869c381827f9
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-25 12:36:51 +0200

    navigator - refactor Screen

--- Modules/NavigatorKit/Sources/Screen.swift
+++ Modules/NavigatorKit/Sources/Screen.swift
@@ -9,16 +9,10 @@
 import Foundation
 import UIKit
 
-public typealias ScreenParams = [String: Any]
-
-/// Screen is used to define a new scene that should be managed by the navigator
-/// A secreen can me pushed, presented, render as a root ... etc
-public protocol Screen {
-    func viewController(with params: ScreenParams?) -> UIViewController
-}
-
-public extension Screen {
-    func viewController() -> UIViewController {
-        viewController(with: [:])
+public struct Screen {
+    public let build: () -> UIViewController
+    
+    public init(_ builder: @escaping () -> UIViewController) {
+        self.build = builder
     }
 }

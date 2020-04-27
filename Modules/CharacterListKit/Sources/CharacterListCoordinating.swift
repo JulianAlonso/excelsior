a9a6d779298911316bdd66b2b5fd5fc22a3f5aa7
Julian Alonso <julian.alonso.dev@gmail.com>
2020-04-27 21:57:23 +0200

    di - created character list coordinating

---
+++ Modules/CharacterListKit/Sources/CharacterListCoordinating.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterListCoordinator.swift
+//  CharacterListKit
+//
+//  Created by Juli Alonso on 27/04/2020.
+//
+
+import Foundation
+import Core
+
+public protocol CharacterListCoordinating {
+    func characterDetail(id: Int)
+}
